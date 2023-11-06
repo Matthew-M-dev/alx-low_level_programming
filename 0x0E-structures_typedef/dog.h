92138693c89dcd712dcba44e8aa1b64cfebe03b0
Matthew-M-dev <muyiwamatthew006@gmail.com>
2023-11-06 09:38:51 -0800

    all good

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * struct dog - struct dog
+ * @name: name
+ * @age: age
+ * @owner: owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif

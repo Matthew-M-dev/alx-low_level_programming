af390b69d627dc7b38d0c927595a5f3b3c1e4b42
Matthew-M-dev <muyiwamatthew006@gmail.com>
2023-11-07 04:51:33 -0800

    all good

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * init_dog - main entry
+ * @d: input
+ * @name: name
+ * @age: age
+ * @owner: owner
+ * Return: No retune type
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{ 
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}

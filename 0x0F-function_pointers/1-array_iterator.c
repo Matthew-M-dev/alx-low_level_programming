a82b7c879dd189d2930d8c21a266ef06e122ec58
Matthew-M-dev <muyiwamatthew006@gmail.com>
2023-11-10 02:10:36 -0800

    all good

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - main entry
+ * @array: input
+ * @size: input
+ * @action: input
+ * Return: Nothing
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i = 0;
+
+	while (i < size)
+	{
+		action(array[i]);
+		i++;
+	}
+}

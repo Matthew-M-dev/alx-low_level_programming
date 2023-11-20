6119e1df67d3ade09db33b1a7fe217f7408af5ac
Matthew-M-dev <muyiwamatthew006@gmail.com>
2023-11-20 14:20:51 -0800

    all good

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * print_list - Write a function that prints all the elements of a list
+ * @h: input
+ * Return: the number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	int i;
+
+	while (h)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
